3aaba84db5f615740b6a536ef0ea7e328528eb06
Andrey <kastolom47@mail.ru>
2026-04-08 08:40:32 +0300

    Первая фиксация Desktop MVVM

---
+++ Rodoslovnaya/ViewModels/MainWindowViewModel.cs
@@ -0,0 +1,6 @@
+﻿namespace Rodoslovnaya.ViewModels;
+
+public partial class MainWindowViewModel : ViewModelBase
+{
+    public string Greeting { get; } = "Welcome to Avalonia!";
+}
---
+++ Rodoslovnaya/Views/MainWindow.axaml.cs
@@ -0,0 +1,11 @@
+using Avalonia.Controls;
+
+namespace Rodoslovnaya.Views;
+
+public partial class MainWindow : Window
+{
+    public MainWindow()
+    {
+        InitializeComponent();
+    }
+}
